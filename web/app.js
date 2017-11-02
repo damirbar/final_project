dec36b8a9d17e9d9956db8dcbab61a24a0ed2acc
DESKTOP-D5S5QEJ\damir <damirbar@gmail.com>
2017-11-02 17:07:45 +0200

    - Mongoose fix

--- web/app.js
+++ web/app.js
@@ -1,10 +1,16 @@
 var express = require("express");
-
+var mongoose = require("mongoose");
 var path = require("path");
 
 var app =express();
 
+var mongoDB = 'mongodb://127.0.0.1/main_db';
+mongoose.connect(mongoDB, {
+    useMongoClien: true
+});
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error!'));
 
 app.get("/",function(req,res){
     console.log(req.headers["x-forwarded-for"] || req.connection.remoteAddress);
