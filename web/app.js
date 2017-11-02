27198bd8c8161ab9749f9c19606a2bfed04905da
stermaneran <stermaneran@gmail.com>
2017-11-02 16:50:45 +0200

    index + profile html page

--- web/app.js
+++ web/app.js
@@ -1,11 +1,22 @@
 var express = require("express");
 
+var path = require("path");
+
 var app =express();
+
+
 
 app.get("/",function(req,res){
     console.log(req.headers["x-forwarded-for"] || req.connection.remoteAddress);
-    res.end("eran");
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
+
+app.get("/profile/:id",function(req,res){
+    console.log(req.headers["x-forwarded-for"] || req.connection.remoteAddress);
+    res.sendFile(path.join(__dirname + "/profile.html"));
+    console.log(req.params.id);
+});
+
 
 app.listen(3000, function(){
     console.log("listening...");
