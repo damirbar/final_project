f52fe74149f71dc92b64a927930e01d9d6679379
Damir Bar <damirbar@gmail.com>
2017-11-09 13:31:00 +0200

    - Routes editted

--- web/app.js
+++ web/app.js
@@ -6,14 +6,17 @@ mongoose.Promise = require("bluebird");
 
 var path = require("path");
 
+// JSON Parsing
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
+// End JSON Parsing
 
 
 var Student = require("./schemas/student");
 var Teacher = require("./schemas/teacher");
+
+var requests = require('./routes/requests');
 
 var feeder = require("./teacherStream");
 
@@ -32,48 +35,54 @@ db.on('error', console.error.bind(console, 'MongoDB connection error!\n'));
 //     console.log("u got "+ student);
 // });
 
-app.post("/student", function(req,res){
-    var myData= new Student(req.body);
-    myData.save()
-        .then(function (item){
-            res.send("successfully saved item to db");
-            console.log("successfully added " +myData.first_name +  " to db");
-    })
-        .catch(function (error) {
-            res.status(400).send("unable to save data. Error: " + error);
-            console.log("unable to save data");
-        });
-});
 
-app.post("/teacher", function(req,res){
-    var myData= new Teacher(req.body);
-    myData.save()
-        .then(function (item){
-            res.send("successfully saved item to db");
-            console.log("successfully added " + myData.first_name +  " to db");
-        })
-        .catch(function (error) {
-            res.status(400).send("unable to save data");
-            console.log("unable to save data");
-        });
-});
 
-app.get("/",function(req,res){
-     console.log(req.headers["x-forwarded-for"] || req.connection.remoteAddress);
-     res.sendFile(path.join(__dirname + "/index.html"));
-});
+app.use('/', requests);
 
+
+
+// app.post("/student", function(req,res){
+//     var myData= new Student(req.body);
+//     myData.save()
+//         .then(function (item){
+//             res.send("successfully saved item to db");
+//             console.log("successfully added " +myData.first_name +  " to db");
+//     })
+//         .catch(function (error) {
+//             res.status(400).send("unable to save data. Error: " + error);
+//             console.log("unable to save data");
+//         });
+// });
+//
+// app.post("/teacher", function(req,res){
+//     var myData= new Teacher(req.body);
+//     myData.save()
+//         .then(function (item){
+//             res.send("successfully saved item to db");
+//             console.log("successfully added " + myData.first_name +  " to db");
+//         })
+//         .catch(function (error) {
+//             res.status(400).send("unable to save data");
+//             console.log("unable to save data");
+//         });
+// });
+//
+// app.get("/",function(req,res){
+//      console.log(req.headers["x-forwarded-for"] || req.connection.remoteAddress);
+//      res.sendFile(path.join(__dirname + "/index.html"));
+// });
+//
+//
+// app.post("/teachers", function(req,res){
+//         feeder.func(req.body,res);
+// });
+//
+// app.get("/teacher", function(req,res){
+//     res.sendFile(path.join(__dirname + "/TestTeacherPage.html"));
+// });
+//
+//
+//
 app.listen(3000, function(){
     console.log("listening...");
 });
-
-app.post("/teachers", function(req,res){
-        feeder.func(req.body,res);
-});
-
-app.get("/teacher", function(req,res){
-    res.sendFile(path.join(__dirname + "/TestTeacherPage.html"));
-});
-
-
-
