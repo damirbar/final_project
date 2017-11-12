f1683a1091f3edea3fd2df7198fd633591f5f7ae
sefi <sefishalom@gmail.com>
2017-11-13 00:35:59 +0200

    Added Courses POST&GET requests. Post and get courses (no filtering yet) at localhost:3000/courses available only via Postman

--- web/routes/requests.js
+++ web/routes/requests.js
@@ -4,6 +4,7 @@ var path = require("path");
 
 var Student = require("../schemas/student");
 var Teacher = require("../schemas/teacher");
+var Course = require("../schemas/course");
 
 
 router.post("/student", function (req, res) {
@@ -133,6 +134,48 @@ router.get("/get-by-name", function (req, res, next) {
             res.json(student);
         });
     }
+});
+
+
+router.post("/courses", function (req, res) {
+    var course = new Course(req.body);
+    course.save(function (err) {
+        if (err) {
+            if (err.name === 'MongoError' && err.code === 11000) {
+                // Duplicate username
+                console.log('Failed adding course Error 11000' + course.name);
+                return res.status(500).send('Failed adding course Error 11000' + course.name);
+            }
+            if (err.name === 'ValidationError') {
+                //ValidationError
+                for (field in err.errors) {
+                    console.log("Validation Error!");
+                    return res.status(500).send("validation Error!");
+                }
+            }
+            // Some other error
+            console.log(err);
+            return res.status(500).send(err);
+        }
+        res.send("successfully added " + course.name + " to db");
+        console.log("successfully added " + course.name + " to db");
+    });
+});
+
+router.get('/courses', function(req,res,next){
+    var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    Course.find({}, function(err,courses){
+        if(err) {
+            console.log("Error while finding courses");
+            return next(err);
+        }else if(courses){
+            console.log(courses);
+            return res.json(courses);
+        }else{
+            console.log("No Courses available");
+            return res.json("No courses available");
+        }
+    });
 });
 
 
