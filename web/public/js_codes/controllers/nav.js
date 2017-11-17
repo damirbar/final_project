6e1166138f4853b5bb7712f9f3a06c7d860f5829
Damir Bar <damirbar@gmail.com>
2017-11-17 17:56:28 +0200

    - More auth progress on navbar-login

--- web/public/js_codes/controllers/nav.js
+++ web/public/js_codes/controllers/nav.js
@@ -1,7 +1,17 @@
 
-wizerApp.controller('navController', ['$scope', 'AuthService', function($scope, AuthService) {
+wizerApp.controller('navController', ['$scope', 'AuthService', '$location', '$timeout',
+    function($scope, AuthService, $location, $timeout) {
 
     $scope.user = {};
+    $scope.hasToken = false;
+
+    if (AuthService.isLoggedIn()) {
+        console.log("Success! User is logged in.");
+        $scope.hasToken = true;
+    } else {
+        console.log("Failure! User is NOT logged in.");
+        $scope.hasToken = false;
+    }
 
     $scope.login = function() {
         AuthService.auth($scope.user.email, $scope.user.password)
@@ -10,6 +20,16 @@ wizerApp.controller('navController', ['$scope', 'AuthService', function($scope, 
             }, function() {
                 console.log("An error occurred");
             });
-    }
+    };
+
+    $scope.logout = function() {
+        console.log("Activated logout function");
+        AuthService.logout();
+        $location.path('/');
+        $timeout(function() {
+            $location.path('/');
+            $scope.hasToken = false;
+        }, 2000);
+    };
 
 }]);
--- web/public/js_codes/services/authService.js
+++ web/public/js_codes/services/authService.js
@@ -1,17 +1,52 @@
-wizerApp.service('AuthService', function($http) {
+wizerApp.service('AuthService', function($http, AuthToken) {
 
     this.auth = function(email, password) {
         return $http.post('/auth-login-user-pass?email=' + email +
         '&password=' + password)
             .then(function(data) {
                 console.log(data.data.msg);
+
+                AuthToken.setToken(data.data.token);
                 return data.data.success;
-                // if (!data.data.success) {
-                //     data.data = data.data.msg;
-                // }
             }, function(){
                 console.log("Error Authenticating " + email);
             });
     };
 
+    // Auth.isLoggedIn()
+    this.isLoggedIn = function() {
+        if (AuthToken.getToken()) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+
+    this.logout = function() {
+        AuthToken.setToken();
+    };
+
+
+})
+
+.factory('AuthToken', function($window) {
+    var authTokenFactory = {};
+
+    // AuthToken.setToken(token);
+    authTokenFactory.setToken = function(token) {
+
+        if (token) {
+            $window.localStorage.setItem('token', token);
+        } else {
+            $window.localStorage.removeItem('token');
+        }
+    };
+
+    // AuthToken.getToken();
+    authTokenFactory.getToken = function() {
+        return $window.localStorage.getItem('token');
+    };
+
+    return authTokenFactory;
 });
