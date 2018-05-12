7662077eb553a8855592b0c86e832900e40ced3d
sefi-work <sefishalom@gmail.com>
2018-05-12 17:00:26 +0300

    rating message is working. users cannot like or disllike a message more than once and can change their rating value.

--- web/schemas/session_message.js
+++ web/schemas/session_message.js
@@ -4,11 +4,14 @@ var SessionMessageSchema = new mongoose.Schema({
     email: String,    // mail of sender
     sid: String ,   // session ID
     type: String,  // question or answer
-    ratings: Array,
+    // raters: [{type: SessionMessageRater, unique: true}],
     likes: {type : Number, default: 0},
     dislikes: {type : Number, default: 0},
+    likers: [String],
+    dislikers: [String],
     body: Array,
     name: String,
     date:Date,
 });
+
 module.exports = mongoose.model('Session_Message', SessionMessageSchema);
