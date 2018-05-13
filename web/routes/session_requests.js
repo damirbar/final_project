fdd5a3b6519ceb6e7b67d811096707bc97f7199d
Unknown <stermaneran@gmail.com>
2018-05-13 15:48:17 +0300

    merge here

--- web/routes/session_requests.js
+++ web/routes/session_requests.js
@@ -298,7 +298,36 @@ router.get("/disconnect", function (req, res) {
 });
 
 
+
+const cloudinary = require('cloudinary');
+
+cloudinary.config({
+    cloud_name: 'wizeup',
+    api_key: '472734726483424',
+    api_secret: 'A2ZBcQsnU72oh7p9JI415BOR1ws'
+});
+
 router.get('/postVideo', function (req, res) {
+    Session.findOne({sid: req.query.sid}, function (err, sess) {
+        if (err) return next(err);
+        cloudinary.v2.uploader.upload("/home/eran/projects/WebstormProjects/final_project/web/routes/good.mp4",
+            { resource_type: "video",
+                public_id: "sessions/" +req.query.sid + 'video',
+                eager: [
+                    { width: 300, height: 300,
+                        crop: "pad", audio_codec: "none" },
+                    { width: 160, height: 100,
+                        crop: "crop", gravity: "south",
+                        audio_codec: "none" } ],
+                eager_async: true,
+                eager_notification_url: "http://mysite/notify_endpoint"},
+            function(error, result) {
+                console.log(result);
+                sess.videoID = result.url;
+                sess.save();
+            });
+    });
+
 
     // good
     // var mongoDB = 'mongodb://damir:damiri@cluster0-shard-00-00-00hhm.mongodb.net:27017,cluster0-shard-00-01-00hhm.mongodb.net:27017,cluster0-shard-00-02-00hhm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
@@ -320,109 +349,117 @@ router.get('/postVideo', function (req, res) {
     // });
     //    good
 
-    const ObjectID = require('mongodb').ObjectID,
-        GridStore = require('mongodb').GridStore,
-        mongoDB = 'mongodb://damir:damiri@cluster0-shard-00-00-00hhm.mongodb.net:27017,cluster0-shard-00-01-00hhm.mongodb.net:27017,cluster0-shard-00-02-00hhm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
-        mongo = require('mongodb');
-
-    mongo.MongoClient.connect(mongoDB, function (err, db) {
-        // Our file ID
-        const fileId = new ObjectID();
-
-        // Open a new file
-        const gridStore = new GridStore(db, fileId, 'session:' + req.query.sid + ' video.mp4', 'w');
-        const source = '/home/eran/projects/WebstormProjects/final_project/web/routes/good.mp4';
-
-        // Open the new file
-        gridStore.open(function (err, gridStore) {
-
-            // Write the file to gridFS
-            console.log("starting to upload file " + source + " as: " + fileId);
-            gridStore.writeFile(source, function (err, doc) {
-                Session.findOne({sid: req.query.sid}, function (err, sess) {
-                    if (err) return next(err);
-                    console.log('saved %s to GridFS file %s', source, fileId);
-                    sess.videoID = fileId;
-                    sess.save();
-                });
-            });
-        });
-    });
+    // const ObjectID = require('mongodb').ObjectID,
+    //     GridStore = require('mongodb').GridStore,
+    //     mongoDB = 'mongodb://damir:damiri@cluster0-shard-00-00-00hhm.mongodb.net:27017,cluster0-shard-00-01-00hhm.mongodb.net:27017,cluster0-shard-00-02-00hhm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+    //     mongo = require('mongodb');
+    //
+    // mongo.MongoClient.connect(mongoDB, function (err, db) {
+    //     // Our file ID
+    //     const fileId = new ObjectID();
+    //
+    //     // Open a new file
+    //     const gridStore = new GridStore(db, fileId, 'session:' + req.query.sid + ' video.mp4', 'w');
+    //     const source = '/home/eran/projects/WebstormProjects/final_project/web/routes/good.mp4';
+    //
+    //     // Open the new file
+    //     gridStore.open(function (err, gridStore) {
+    //
+    //         // Write the file to gridFS
+    //         console.log("starting to upload file " + source + " as: " + fileId);
+    //         gridStore.writeFile(source, function (err, doc) {
+    //             Session.findOne({sid: req.query.sid}, function (err, sess) {
+    //                 if (err) return next(err);
+    //                 console.log('saved %s to GridFS file %s', source, fileId);
+    //                 sess.videoID = fileId;
+    //                 sess.save();
+    //             });
+    //         });
+    //     });
+    // });
 });
 
 
 router.get('/getVideo', function (req, res) {
 
-    const GridStore = require('mongodb').GridStore;
-    const db = mongoose.connection;
-    const ObjectID = require('mongodb').ObjectID;
-    db.options = 'PRIMARY';
+
     Session.findOne({sid: req.query.sid}, function (err, sess) {
         if (err) return next(err);
-        if (sess.videoID !== "") {
-            new GridStore(db, new ObjectID(sess.videoID), 'r').open(function (err, GridFile) {
-                if (!GridFile) {
-                    console.log("video" + " not found!!!");
-                    res.status(404).json({message: "video" + " not found"})
-                }
-                else {
-                    console.log("starting to stream file " + GridFile.filename);
-                    StreamGridFile(req, res, GridFile)
-                }
-            });
-        }
-        else {
-            console.log("video" + " not found!!!");
-            res.status(404).json({message: "video" + " not found"})
+        if (sess) {
+            return sess.videoID;
         }
     });
 
+    // const GridStore = require('mongodb').GridStore;
+    // const db = mongoose.connection;
+    // const ObjectID = require('mongodb').ObjectID;
+    // db.options = 'PRIMARY';
+    // Session.findOne({sid: req.query.sid}, function (err, sess) {
+    //     if (err) return next(err);
+    //     if (sess.videoID !== "") {
+    //         new GridStore(db, new ObjectID(sess.videoID), 'r').open(function (err, GridFile) {
+    //             if (!GridFile) {
+    //                 console.log("video" + " not found!!!");
+    //                 res.status(404).json({message: "video" + " not found"})
+    //             }
+    //             else {
+    //                 console.log("starting to stream file " + GridFile.filename);
+    //                 StreamGridFile(req, res, GridFile)
+    //             }
+    //         });
+    //     }
+    //     else {
+    //         console.log("video" + " not found!!!");
+    //         res.status(404).json({message: "video" + " not found"})
+    //     }
+    // });
+
 });
 
-function StreamGridFile(req, res, GridFile) {
-    if (req.headers['range']) {
-        // Range request, partialle stream the file
-        console.log('Range Reuqest');
-        const parts = req.headers['range'].replace(/bytes=/, "").split("-");
-        const partialstart = parts[0];
-        const partialend = parts[1];
-        let start = parseInt(partialstart, 10);
-        const end = partialend ? parseInt(partialend, 10) : GridFile.length - 1;
-        const chunksize = (end - start) + 1;
-        console.log('Range ', start, '-', end);
-        res.writeHead(206, {
-            'Content-Range': 'bytes ' + start + '-' + end + '/' + GridFile.length,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': GridFile.contentType
-        });
-        // Set filepointer
-        GridFile.seek(start, function () {
-            // get GridFile stream
-            const stream = GridFile.stream(true);
-            // write to response
-            stream.on('data', function (buff) {
-                // count data to abort streaming if range-end is reached
-                // perhaps theres a better way?
-                start += buff.length - 1;
-                if (start >= end) {
-                    // enough data send, abort
-                    GridFile.close();
-                    res.end();
-                } else {
-                    res.write(buff);
-                }
-            });
-        });
-    } else {
-        // stream back whole file
-        console.log('No Range Request');
-        res.header('Content-Type', GridFile.contentType);
-        res.header('Content-Length', GridFile.length);
-        const stream = GridFile.stream(true);
-        stream.pipe(res);
-    }
-}
+// function StreamGridFile(req, res, GridFile) {
+//     if (req.headers['range']) {
+//         // Range request, partialle stream the file
+//         console.log('Range Reuqest');
+//         const parts = req.headers['range'].replace(/bytes=/, "").split("-");
+//         const partialstart = parts[0];
+//         const partialend = parts[1];
+//         let start = parseInt(partialstart, 10);
+//         const end = partialend ? parseInt(partialend, 10) : GridFile.length - 1;
+//         const chunksize = (end - start) + 1;
+//         console.log('Range ', start, '-', end);
+//         res.writeHead(206, {
+//             'Content-Range': 'bytes ' + start + '-' + end + '/' + GridFile.length,
+//             'Accept-Ranges': 'bytes',
+//             'Content-Length': chunksize,
+//             'Content-Type': GridFile.contentType
+//         });
+//         // Set filepointer
+//         GridFile.seek(start, function () {
+//             // get GridFile stream
+//             const stream = GridFile.stream(true);
+//             // write to response
+//             stream.on('data', function (buff) {
+//                 // count data to abort streaming if range-end is reached
+//                 // perhaps theres a better way?
+//                 start += buff.length - 1;
+//                 if (start >= end) {
+//                     // enough data send, abort
+//                     GridFile.close();
+//                     res.end();
+//                 } else {
+//                     res.write(buff);
+//                 }
+//             });
+//         });
+//     } else {
+//         // stream back whole file
+//         console.log('No Range Request');
+//         res.header('Content-Type', GridFile.contentType);
+//         res.header('Content-Length', GridFile.length);
+//         const stream = GridFile.stream(true);
+//         stream.pipe(res);
+//     }
+// }
 
 
 module.exports = router;
