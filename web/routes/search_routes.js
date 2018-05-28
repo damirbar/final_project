dec9ab2f30d3ef4b4025c7f7080fe055061be1fa
sefi-work <sefishalom@gmail.com>
2018-05-28 12:27:04 +0300

    Free text search applied on Users, Sessions and Files. Still working on the others

--- web/routes/search_routes.js
+++ web/routes/search_routes.js
@@ -2,16 +2,112 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require("mongoose");
 var User = require("../schemas/user");
+var File = require("../schemas/file");
+var Session = require("../schemas/session");
+var University = require("../schemas/university");
+var Course = require("../schemas/course");
+var Faculty = require("../schemas/faculty");
+var Department = require("../schemas/department");
+
 
 router.get('/free-text-search', function (req, res) {
-    console.log('free text search');
-    let keywords = req.query.keywords;
+
+    let keywords =  req.query.keyword;
+
+    let numOfCollections = 3;
+    let doneCounter = 0;
+
+    let searchResults = {};
+
+    let sendSearchResults = function(){
+        // doneCounter = 0;
+        res.status(200).json(searchResults);
+    };
 
     User.find({$text: {$search: keywords}}, function (err, results) {
-        if (err) return err;
-        console.log(results);
-        return res.status(200).json({message: 'free text search', results: results});
-        });
+        if(err){
+            doneCounter++;
+            console.log(err);
+
+        }else{
+            searchResults.users = results;
+            doneCounter++;
+            if(doneCounter == numOfCollections){
+                sendSearchResults();
+            }
+        }
+    });
+
+    File.find({$text: {$search: keywords}}, function (err, results) {
+        if(err){
+            console.log(err);
+            doneCounter++;
+        }else{
+            searchResults.files = results;
+            doneCounter++;
+            if(doneCounter == numOfCollections){
+                sendSearchResults();
+            }
+        }
+    });
+
+    Session.find({$text: {$search: keywords}}, function (err, results) {
+        if(err){
+            console.log(err);
+            doneCounter++;
+        }else{
+            searchResults.sessions = results;
+            doneCounter++;
+            if(doneCounter == numOfCollections){
+                sendSearchResults();
+            }
+        }
+    });
+
+    // User.find({$text: {$search: keywords}}, function (err, results) {
+    //     if(err){
+    //         console.log(err);
+    //     }else{
+    //         doneCounter++;
+    //         if(doneCounter == 7){
+    //             sendSearchResults();
+    //         }
+    //     }
+    // });
+
+    // User.find({$text: {$search: keywords}}, function (err, results) {
+    //     if(err){
+    //         console.log(err);
+    //     }else{
+    //         doneCounter++;
+    //         if(doneCounter == 7){
+    //             sendSearchResults();
+    //         }
+    //     }
+    // });
+    //
+    // User.find({$text: {$search: keywords}}, function (err, results) {
+    //     if(err){
+    //         console.log(err);
+    //     }else{
+    //         doneCounter++;
+    //         if(doneCounter == 7){
+    //             sendSearchResults();
+    //         }
+    //     }
+    // });
+    //
+    // User.find({$text: {$search: keywords}}, function (err, results) {
+    //     if(err){
+    //         console.log(err);
+    //     }else{
+    //         doneCounter++;
+    //         if(doneCounter == 7){
+    //             sendSearchResults();
+    //         }
+    //     }
+    // });
 });
+
 
 module.exports = router;
