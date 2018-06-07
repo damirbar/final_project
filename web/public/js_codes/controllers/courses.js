a837872b4a0cb7c152784aa32a0030e249669c3e
Damir Bar <damirbar@gmail.com>
2018-06-08 00:08:59 +0300

    - Full courses page. Link from navbar. - Added loading effect while fetching courses. - Started implementing specific course page.

--- web/public/js_codes/controllers/courses.js
+++ web/public/js_codes/controllers/courses.js
@@ -2,14 +2,17 @@ wizerApp.controller('coursesController',
     function ($scope, $routeParams, $location, CourseService) {
 
     $scope.courses = [];
+    $scope.loading = true;
 
     $scope.getMyCourses = function() {
         CourseService.getMyCourses()
             .then(function (data) {
                 $scope.courses = data;
                 console.log("Courses = " + JSON.stringify(data));
+                $scope.loading = false;
             }, function(){
                 console.log("Error getting my courses");
+                $scope.loading = false;
             })
     };
 
--- web/public/js_codes/wizerApp.js
+++ web/public/js_codes/wizerApp.js
@@ -25,10 +25,10 @@ wizerApp.config(function ($routeProvider, $locationProvider) {
             controller: 'getProfilesController'
         })
 
-        .when('/courses',{
-            templateUrl:'../pages/courses.html',
-            controller:'courseController'
-        })
+        // .when('/courses',{
+        //     templateUrl:'../pages/courses.html',
+        //     controller:'courseController'
+        // })
 
         .when('/courses/search-by-name/:course_name', {
             templateUrl: '../pages/courses.html',
@@ -48,6 +48,11 @@ wizerApp.config(function ($routeProvider, $locationProvider) {
         .when('/courses/', {
             templateUrl: '../pages/courses.html',
             controller: 'coursesController'
+        })
+
+        .when('/courses/:id', {
+            templateUrl: '../pages/course.html',
+            controller: 'courseController'
         })
 
     .otherwise({ redirectTo: '/'});
