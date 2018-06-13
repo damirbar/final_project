688aae3d9b21fe48afc323d854bcb0fa9d6e3cd4
Damir Bar <damirbar@gmail.com>
2018-06-13 18:49:36 +0300

    - Search page fully functional

--- web/public/js_codes/controllers/home.js
+++ web/public/js_codes/controllers/home.js
@@ -1,6 +1,15 @@
-wizerApp.controller('homeController', ['$scope', '$http', 'SessionService',
-    function ($scope, $http ,SessionService) {
-}]);
+wizerApp.controller('homeController', ['$scope', '$http', 'SessionService', 'SearchService', '$location',
+    function ($scope, $http, SessionService, SearchService, $location) {
+
+        //Free text search
+        $scope.searchTerm = {keywords: ""}; //keywords for the free text search
+
+        $scope.freeTextSearch = function () {
+            console.log($scope.searchTerm.keywords);
+            // SearchService.freeTextSearch($scope.searchTerm.keywords);
+            $location.path("/search/" + $scope.searchTerm.keywords);
+        };
+    }]);
 
 
 
