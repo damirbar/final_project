06378d0ae88dca4a9b759c97ad922492fb1ab182
Damir Bar <damirbar@gmail.com>
2018-06-14 18:46:48 +0300

    - Search results remastered

--- web/public/js_codes/controllers/search.js
+++ web/public/js_codes/controllers/search.js
@@ -1,10 +1,12 @@
 wizerApp.controller('searchController',
     function ($scope, $routeParams, ProfileService, SearchService) {
 
+    $scope.loading = true;
         $scope.results = {};
         SearchService.freeTextSearch($routeParams.keyword)
             .then(function (data) {
                 $scope.results = data;
+                $scope.loading = true;
                 console.log(data);
             }, function (err) {
 
