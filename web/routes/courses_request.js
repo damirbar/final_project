11d3e6f4d530e7f440364719833a9f4b086f9fbd
Damir Bar <damirbar@gmail.com>
2018-06-14 22:01:04 +0300

    - Fixed error with "get-messages" of course without messages

--- web/routes/courses_request.js
+++ web/routes/courses_request.js
@@ -329,7 +329,11 @@ router.get("/get-all-messages", function (req, res) {
                 console.log(err);
                 return err;
             } else {
-                return res.status(200).json(list[0].messages_list);
+                if (list[0]) {
+                    return res.status(200).json(list[0].messages_list);
+                } else {
+                    return res.status(200).json([]);
+                }
             }
         }
     );
