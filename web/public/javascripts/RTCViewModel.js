81750fe1a288bb124aa904f2b9af8c46e4019482
stermaneran <stermaneran@gmail.com>
2018-09-14 15:25:22 +0300

    z

--- web/public/javascripts/RTCViewModel.js
+++ web/public/javascripts/RTCViewModel.js
@@ -1,3 +1,18 @@
+// Model
+var RTCStream = function(id, data) {
+    this.id = id;
+    this.name = ko.observable();
+    this.isPlaying = ko.observable(false);
+
+    this.update(data);
+};
+
+ko.utils.extend(RTCStream.prototype, {
+    update: function(data) {
+        this.name(data.name);
+    }
+});
+
 // View Model
 var RTCViewModel = function(client, path) {
     var client = client,
@@ -9,25 +24,64 @@ var RTCViewModel = function(client, path) {
                 optional: []
             }
         },
-        availableStreams = {},
+        availableStreams = ko.observable([]),
         isStreaming = ko.observable(false),
+        name = ko.observable('Guest'),
         link = ko.observable(),
         localVideoEl = document.getElementById('localVideo');
+
+    // push changes to server
+    ko.computed(function() {
+        if(isStreaming()) {
+            client.send('update', {
+                name: name()
+            });
+        }
+    }).extend({throttle: 500});
 
     function getReadyToStream(stream) {
         attachMediaStream(localVideoEl, stream);
         client.setLocalStream(stream);
-        client.send('readyToStream');
+        client.send('readyToStream', {
+                name: name()
+            }
+        );
         link(window.location.host + "/" + client.getId());
         isStreaming(true);
     }
+    function getStreamById(id) {
+        for(var i=0; i<availableStreams().length;i++) {
+            if (availableStreams()[i].id === id) {return availableStreams()[i];}
+        }
+    }
     function loadStreamsFromServer() {
-        availableStreams ={}
+        // Load JSON data from server
+        $.getJSON(path, function(data) {
+
+            var mappedStreams = [];
+            for(var remoteId in data) {
+                var stream = getStreamById(remoteId);
+
+                // if stream is known, keep its state and update it
+                if(!!stream) {
+                    stream.update(data[remoteId]);
+                    mappedStreams.push(stream);
+                    // else create a new stream (escape own stream)
+                } else {
+                    if(remoteId !== client.getId()) {
+                        mappedStreams.push(new RTCStream(remoteId, data[remoteId]));
+                    }
+                }
+            }
+
+            availableStreams(mappedStreams);
+        });
     }
 
     return {
         streams: availableStreams,
         isStreaming: isStreaming,
+        name: name,
         link: link,
         localCamButtonText: ko.computed(
             function() {
@@ -38,7 +92,7 @@ var RTCViewModel = function(client, path) {
         refresh: loadStreamsFromServer,
         toggleLocalVideo: function() {
             if(isStreaming()){
-                client.send('stopStreaming');
+                client.send('leave');
                 localVideoEl.src = '';
                 client.setLocalStream(null);
                 isStreaming(false);
@@ -48,5 +102,24 @@ var RTCViewModel = function(client, path) {
                 });
             }
         },
+        toggleRemoteVideo: function(stream) {
+            client.peerInit(stream.id);
+            stream.isPlaying(!stream.isPlaying());
+        },
+
+        answerCall: function(stream) {
+            var remoteId = stream.id || stream;
+            getUserMedia(
+                mediaConfig,
+                function (strm) {
+                    getReadyToStream(strm);
+                    client.pushStream(remoteId);
+                    getStreamById(remoteId).isPlaying(true);
+                },
+                function () {
+                    throw new Error('Failed to get access to local media.');
+                }
+            );
+        }
     }
 };
