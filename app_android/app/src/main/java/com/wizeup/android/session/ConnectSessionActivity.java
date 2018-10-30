c3cb88becf75a9dc72beb26d6a5d85538128e34a
shay <shayc91@gmail.com>
2018-10-30 16:25:08 +0200

    Notification fix bug

--- app_android/app/src/main/java/com/wizeup/android/session/ConnectSessionActivity.java
+++ app_android/app/src/main/java/com/wizeup/android/session/ConnectSessionActivity.java
@@ -76,7 +76,7 @@ public class ConnectSessionActivity extends AppCompatActivity {
 
     private void createSession() {
         this.startActivity(new Intent(this, CreateSessionActivity.class));
-
+        finish();
     }
 
 
